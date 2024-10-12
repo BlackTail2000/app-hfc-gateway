343ed556c3f6fe2b2e7f4f461b0219812348e302
BlackTail2000 <carlosgabrielbacamanrique@hotmail.com>
2024-10-12 00:30:23 -0500

    Proyecto Creado

---
+++ src/test/java/pe/edu/cibertec/app_hfc_gateway/AppHfcGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.edu.cibertec.app_hfc_gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppHfcGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
